84503fe334fa622a0c1600d5ba2c1bf67ae011d3
x <songbai_x@qq.com>
2021-03-25 15:40:45 +0800

    大致布局

---
+++ src/layout/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">
+        <TreeList> </TreeList>
+      </el-aside>
+      <el-container>
+        <el-header>
+          <TagNav></TagNav>
+        </el-header>
+        <el-main>
+          <router-view />
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+    <el-footer>Footer</el-footer>
+  </div>
+</template>
+
+<script>
+import TagNav from "./components/tag-nav";
+import TreeList from "./components/tree-list";
+export default {
+  components: {
+    TagNav,
+    TreeList,
+  },
+};
+</script>
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
