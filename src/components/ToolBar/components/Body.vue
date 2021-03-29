9baa5185d07bee221b38c806c912dc597c2ac585
x <songbai_x@qq.com>
2021-03-29 15:22:53 +0800

    布局

---
+++ src/components/ToolBar/components/Body.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-card class="body-area">
+    <template #header>
+      <div class="card-header">
+        <span>Body</span>
+        <el-dropdown size="large">
+          <span class="el-dropdown-link">
+            ContentType<i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item>黄金糕</el-dropdown-item>
+              <el-dropdown-item>狮子头</el-dropdown-item>
+              <el-dropdown-item>螺蛳粉</el-dropdown-item>
+              <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+              <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </div>
+    </template>
+    <div v-for="o in 4" :key="o" class="text item">
+      {{ "列表内容 " + o }}
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.body-area {
+  .card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .box-card {
+    width: 100%;
+  }
+
+  .el-card__header {
+    padding: 9px 10px;
+  }
+}
+</style>
+
--- src/components/ToolBar/index.vue
+++ src/components/ToolBar/index.vue
@@ -1,13 +1,25 @@
 <template>
   <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
-    <el-tab-pane label="Params" name="first">用户管理</el-tab-pane>
-    <el-tab-pane label="Auth" name="second">配置管理</el-tab-pane>
-    <el-tab-pane label="Headers" name="third">角色管理</el-tab-pane>
-    <el-tab-pane label="Body" name="fourth">定时任务补偿</el-tab-pane>
+    <el-tab-pane label="Params" name="first">
+      <Params></Params>
+    </el-tab-pane>
+    <el-tab-pane label="Auth" name="second">认证</el-tab-pane>
+    <el-tab-pane label="Headers" name="third">
+      <Params></Params>
+    </el-tab-pane>
+    <el-tab-pane label="Body" name="fourth">
+      <Body></Body>
+    </el-tab-pane>
   </el-tabs>
 </template>
 <script>
+import Params from "./components/Params";
+import Body from "./components/Body";
 export default {
+  components: {
+    Params,
+    Body,
+  },
   data() {
     return {
       activeName: "first",
