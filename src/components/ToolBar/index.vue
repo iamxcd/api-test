67570ed2680b7f99fba410f92c29bb1fd564e336
x <songbai_x@qq.com>
2021-03-29 14:25:31 +0800

    目录结构调整

---
+++ src/components/ToolBar/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+    <el-tab-pane label="Params" name="first">用户管理</el-tab-pane>
+    <el-tab-pane label="Auth" name="second">配置管理</el-tab-pane>
+    <el-tab-pane label="Headers" name="third">角色管理</el-tab-pane>
+    <el-tab-pane label="Body" name="fourth">定时任务补偿</el-tab-pane>
+  </el-tabs>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      activeName: "first",
+    };
+  },
+  methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/api/index.vue
+++ src/views/api/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="api">
     <UrlBar></UrlBar>
+    <ToolBar></ToolBar>
   </div>
 </template>
 
 <script>
-import UrlBar from "./components/UrlBar";
+import UrlBar from "@/components/UrlBar";
+import ToolBar from "@/components/ToolBar";
 export default {
   name: "api",
   components: {
     UrlBar,
+    ToolBar,
   },
   computed: {
     key() {
