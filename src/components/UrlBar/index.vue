87d204906be60e308f9815ba4d9dedc40ce66f58
x <songbai_x@qq.com>
2021-04-13 16:17:50 +0800

    第一个请求完成

--- src/components/UrlBar/index.vue
+++ src/components/UrlBar/index.vue
@@ -1,11 +1,7 @@
 <template>
-  <el-form :inline="true" size="large" :model="formInline" class="url-bar">
+  <el-form :inline="true" size="large" :model="form" class="url-bar">
     <el-form-item class="ipt">
-      <el-input
-        v-model="formInline.user"
-        class="url_ipt"
-        placeholder="请输入URL"
-      >
+      <el-input v-model="form.url" class="url_ipt" placeholder="请输入URL">
         <template #prepend>
           <el-dropdown
             class="action-dropdown"
@@ -14,14 +10,14 @@
           >
             <div class="action">
               <div class="action_name">
-                {{ curAction }}
+                {{ form.method }}
               </div>
               <i class="el-icon-arrow-down el-icon--right"></i>
             </div>
             <template #dropdown>
               <el-dropdown-menu>
                 <el-dropdown-item
-                  v-for="(action, index) in actions"
+                  v-for="(action, index) in methods"
                   :key="index"
                   :command="action"
                   >{{ action }}</el-dropdown-item
@@ -33,40 +29,66 @@
       </el-input>
     </el-form-item>
     <el-form-item class="btn">
-      <el-button type="primary" class="btn-send" @click="onSubmit"
+      <el-button type="primary" class="btn-send" @click="onSend()"
         >发送</el-button
       >
-      <el-dropdown size="large">
-        <el-button type="primary">
-          保存<i class="el-icon-arrow-down el-icon--right btn-save"></i>
-        </el-button>
-        <template #dropdown>
+      <!-- <el-dropdown size="large"> -->
+      <el-button type="primary" @click="onSave()">
+        保存
+        <!-- <i class="el-icon-arrow-down el-icon--right btn-save"></i> -->
+      </el-button>
+      <!-- <template #dropdown>
           <el-dropdown-menu>
             <el-dropdown-item>另存为</el-dropdown-item>
           </el-dropdown-menu>
-        </template>
-      </el-dropdown>
+        </template> -->
+      <!-- </el-dropdown> -->
     </el-form-item>
   </el-form>
 </template>
 <script>
 export default {
+  props: {
+    url: {
+      type: String,
+      default: "",
+    },
+    method: {
+      type: String,
+      default: "GET",
+    },
+  },
   data() {
     return {
-      formInline: {
-        user: "",
-        region: "",
+      form: {
+        url: "",
+        method: "GET",
       },
-      curAction: "GET",
-      actions: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     };
+  },
+  created() {
+    this.form.url = this.url;
+    this.form.method = this.method;
   },
   methods: {
     handleCommand(action) {
-      this.curAction = action;
-      console.log(action);
+      this.form.method = action;
     },
-    onSubmit() {},
+    onSend() {
+      this.$emit("onSend");
+    },
+    onSave() {
+      this.$emit("onSave");
+    },
+  },
+  watch: {
+    form: {
+      handler(val) {
+        this.$emit("onChange", val);
+      },
+      deep: true,
+    },
   },
 };
 </script>
--- src/views/api/index.vue
+++ src/views/api/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="api">
-    <UrlBar></UrlBar>
+    <UrlBar
+      :url="request.url"
+      :method="request.method"
+      @onChange="urlChange"
+      @onSave="onSave"
+      @onSend="onSend"
+    ></UrlBar>
     <ToolBar></ToolBar>
   </div>
 </template>
@@ -8,15 +14,48 @@
 <script>
 import UrlBar from "@/components/UrlBar";
 import ToolBar from "@/components/ToolBar";
+import http from "@/libs/request";
 export default {
   name: "api",
   components: {
     UrlBar,
     ToolBar,
   },
+  data() {
+    return {
+      request: {
+        url: "",
+        method: "GET",
+      },
+    };
+  },
   computed: {
     key() {
       return this.$route.params.key;
+    },
+  },
+  methods: {
+    urlChange(obj) {
+      this.request.url = obj.url;
+      this.request.method = obj.method;
+      console.log(this.request);
+    },
+    onSave() {
+      // TODO 保存
+    },
+    onSend() {
+      console.log(this.request);
+      if (this.request.url == "") {
+        this.$message.error("请输入URL");
+        return;
+      }
+      // 发送请求
+      http({
+        method: this.request.methods,
+        url: this.request.url,
+      }).then((res) => {
+        console.log(res);
+      });
     },
   },
 };
