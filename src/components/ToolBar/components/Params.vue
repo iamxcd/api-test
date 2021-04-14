e871cfc9f5cd8805e5e682d92172fb30a0e0d1ba
x <songbai_x@qq.com>
2021-04-14 16:38:44 +0800

    选中和删除功能完成

--- src/components/ToolBar/components/Params.vue
+++ src/components/ToolBar/components/Params.vue
@@ -3,9 +3,15 @@
     border
     :data="tableData"
     style="width: 100%"
+    :row-style="{ height: '0' }"
+    :cell-style="{ padding: '0' }"
     @cell-click="cellClick"
   >
-    <el-table-column prop="id" width="40"> </el-table-column>
+    <el-table-column prop="id" width="40">
+      <template #default="{ row }">
+        <el-checkbox v-model="row.is_enable"></el-checkbox>
+      </template>
+    </el-table-column>
     <el-table-column prop="name" label="参数名" width="180">
       <template #default="{ row }">
         <input
@@ -44,46 +50,71 @@
       </template>
     </el-table-column>
     <el-table-column prop="intro" label="描述">
-      <template #default="{ row }">
-        <input
-          v-if="row[rename_key + 'intro']"
-          type="text"
-          v-focus
-          v-model="row.intro"
-          @keyup.enter="renameHandle(row, 'intro')"
-          @blur="renameHandle(row, 'intro')"
-          class="rename_input"
-        />
-        <div v-else>
-          {{ row.intro }}
+      <template #default="{ row, $index }">
+        <div class="intro">
+          <input
+            v-if="row[rename_key + 'intro']"
+            type="text"
+            v-focus
+            v-model="row.intro"
+            @keyup.enter="renameHandle(row, 'intro')"
+            @blur="renameHandle(row, 'intro')"
+            class="rename_input left"
+          />
+          <div class="left" v-else>
+            {{ row.intro }}
+          </div>
+          <i
+            class="el-icon-delete del-right"
+            @click.stop="onClickDel($index)"
+          ></i>
         </div>
       </template>
     </el-table-column>
   </el-table>
 </template>
 <script>
+import _ from "lodash";
 export default {
   data() {
     return {
-      tableData: [
-        {
-          name: "姓名",
-          value: "王小虎",
-          required: false,
-          intro: "上海市普陀区金沙江路 1518 弄",
-        },
-      ],
+      tableData: [],
+      rowData: {
+        is_enable: true,
+        name: "",
+        value: "",
+        required: false,
+        intro: "",
+      },
       rename_key: "$_isShowInput_",
     };
+  },
+  created() {
+    this.tableData.push(_.clone(this.rowData));
   },
   methods: {
     renameHandle(data, column) {
       delete data[this.rename_key + column];
-      console.log(data);
+      // console.log(data);
     },
     cellClick(row, column) {
-      let p = this.rename_key + column.property;
-      row[p] = true;
+      let property = this.rename_key + column.property;
+      row[property] = true;
+    },
+    onClickDel(index) {
+      this.tableData.splice(index, 1);
+    },
+  },
+  watch: {
+    tableData: {
+      handler(arr) {
+        let len = arr.length;
+        if (len == 0 || arr[len - 1].name != "") {
+          this.tableData.push(_.clone(this.rowData));
+        }
+        this.$emit("change", arr);
+      },
+      deep: true,
     },
   },
 };
@@ -92,5 +123,24 @@ export default {
 <style lang="scss" scoped>
 .rename_input {
   width: 90%;
+}
+.intro {
+  display: flex;
+  align-items: center;
+  .left {
+    flex: 1;
+  }
+  .del-right {
+    display: none;
+  }
+}
+</style>
+<style  lang="scss">
+.el-table__row {
+  &:hover {
+    .del-right {
+      display: block;
+    }
+  }
 }
 </style>
