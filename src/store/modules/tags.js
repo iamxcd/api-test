05fe833e0cfb1693d30d85f43e82b5b7b58e0462
x <songbai_x@qq.com>
2021-04-15 15:16:31 +0800

    提示未保存

--- src/store/modules/tags.js
+++ src/store/modules/tags.js
@@ -1,12 +1,33 @@
 import router from "@/router"
 import _ from 'lodash'
 import uuid from '@/plugins/uuid'
+import { ElMessageBox } from 'element-plus';
+
+
+function closeTag(index, state, key) {
+    state.tagList.splice(index, 1)
+    if (router.currentRoute._value.params.key == key) {
+        let path = '/'
+        if (state.tagList.length != 0) {
+            if (index == 0) {
+                let _key = state.tagList[0].key
+                path = `/api/${_key}`
+            } else {
+                let _key = state.tagList[index - 1].key
+                path = `/api/${_key}`
+            }
+
+        }
+        router.push({ path: path })
+    }
+}
 
 const state = {
     tagList: [
         // {
         //     title: '接口名',
-        //     key: '3320cc226f264022a457c5e23f6cbba5'
+        //     key: '3320cc226f264022a457c5e23f6cbba5',
+        //     is_saved: true
         // }
     ],
 }
@@ -17,20 +38,19 @@ const mutations = {
     CLOSE_TAG(state, key) {
         let index = _.findIndex(state.tagList, { 'key': key })
         if (index > -1) {
-            state.tagList.splice(index, 1)
-            if (router.currentRoute._value.params.key == key) {
-                let path = '/'
-                if (state.tagList.length != 0) {
-                    if (index == 0) {
-                        let _key = state.tagList[0].key
-                        path = `/api/${_key}`
-                    } else {
-                        let _key = state.tagList[index - 1].key
-                        path = `/api/${_key}`
-                    }
-
-                }
-                router.push({ path: path })
+            let tag = state.tagList[index]
+            if (!tag.is_saved) {
+                ElMessageBox.confirm('当前接口未保存, 是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    closeTag(index, state, key)
+                }).catch(() => {
+                    console.log('取消关闭')
+                });
+            } else {
+                closeTag(index, state, key)
             }
         }
         console.log('index:', index);
@@ -40,6 +60,7 @@ const mutations = {
         state.tagList.push({
             title: "未命名",
             key: key,
+            is_saved: false
         });
         router.push({ path: `/api/${key}` })
     },
@@ -52,7 +73,8 @@ const mutations = {
         if (!isset) {
             state.tagList.unshift({
                 title,
-                key
+                key,
+                is_saved: true
             })
         }
         router.push(`/api/${key}`)
